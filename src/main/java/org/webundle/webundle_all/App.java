547d8a51373db27e8c3a4be0c4a8a7ddf1ac8308
syu <yusong198412@163.com>
2014-02-18 16:05:31 +0800

    init webundle project

---
+++ src/main/java/org/webundle/webundle_all/App.java
@@ -0,0 +1,13 @@
+package org.webundle.webundle_all;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
